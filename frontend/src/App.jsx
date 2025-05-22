7fd3cb79fba3f5612f1f28d49a7bcdc7815963f8
joy935 <tupaiv@gmail.com>
2025-05-22 12:14:33 +0200

    added header and updated navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,28 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import NavBar from './components/NavBar';
+import Header from './components/Header';
 
 import Home from './pages/Home';
 import Dashboard from './pages/Dashboard';
 import Accounts from './pages/Accounts';
+import Transactions from './pages/Transactions';
 
 function App() {
   return (
     <Router>
-      <NavBar />
-      <div className="p-4">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/accounts" element={<Accounts />} />
-        </Routes>
+      <div className="flex">
+        <NavBar />
+        <div className="flex-1">
+          <Header />
+          <main className="mt-16 ml-64 p-6">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/dashboard" element={<Dashboard />} />
+              <Route path="/accounts" element={<Accounts />} />
+              <Route path="/transactions" element={<Transactions />} />
+            </Routes>
+          </main>
+        </div>
       </div>
     </Router>
   );
