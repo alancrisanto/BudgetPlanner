3a9417ea3a1df56eec43e58d5273bbf2818cd2fb
CWP <77127867+Camwp@users.noreply.github.com>
2025-06-07 16:21:03 -0700

    dashboard api user state fix

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -8,7 +8,7 @@ import CategoriesMonthsChart from '../components/charts/CategoriesMonthsChart';
 const VITE_API_URL = import.meta.env.VITE_API_URL;
 
 const Dashboard = () => {
-	const user = JSON.parse(localStorage.getItem("user"));
+	const [user, setUser] = useState(() => JSON.parse(localStorage.getItem("user")));
 
 	const { isAuthenticated } = useAuth();
 	const [loading, setLoading] = useState(true);
@@ -18,7 +18,7 @@ const Dashboard = () => {
 	const [recentTransactions, setRecentTransactions] = useState([]);
 
 	useEffect(() => {
-		if (!isAuthenticated) { return; }
+		if (!isAuthenticated || !user) return;
 
 		const fetchData = async () => {
 			try {
@@ -51,7 +51,7 @@ const Dashboard = () => {
 					headers: {
 						Authorization: `Bearer ${user.token}`,
 					},
-				});	
+				});
 				if (!allTransactionsResponse.ok) {
 					throw new Error("Failed to fetch all transactions");
 				}
@@ -65,7 +65,7 @@ const Dashboard = () => {
 			}
 		};
 		fetchData();
-	}, [isAuthenticated, user]);
+	}, [isAuthenticated, user?.token]);
 
 	return (
 		<div>
@@ -78,103 +78,103 @@ const Dashboard = () => {
 			<p className="text-gray-700">This is your Financial Overview Report</p>
 
 
-				{loading ? (
+			{loading ? (
 				<div className="flex items-center justify-center h-64">
 					<p className="text-gray-500">Loading...</p>
 				</div>
-				) : error ? (
+			) : error ? (
 				<div className="flex items-center justify-center h-64">
 					<p className="text-red-500">{error}</p>
 				</div>
-				) : (
+			) : (
 				<div className="flex flex-col lg:flex-row gap-6 mt-6">
 					{/* Balances Card */}
 					{accounts.length > 0 ? (
-					<div className="bg-white shadow-md rounded-xl p-6 border border-gray-200 flex-1">
-						<h3 className="text-xl font-semibold mb-4">My Accounts</h3>
-						{accounts.map((account) => (
-							<div key={account._id} className="flex justify-between items-start py-4">
-							{/* Account name on the left */}
-							<div className="text-lg font-medium text-gray-800 w-1/2">
-								{account.name}
-							</div>
+						<div className="bg-white shadow-md rounded-xl p-6 border border-gray-200 flex-1">
+							<h3 className="text-xl font-semibold mb-4">My Accounts</h3>
+							{accounts.map((account) => (
+								<div key={account._id} className="flex justify-between items-start py-4">
+									{/* Account name on the left */}
+									<div className="text-lg font-medium text-gray-800 w-1/2">
+										{account.name}
+									</div>
 
-							{/* Numbers on the right */}
-							<div className="text-right w-1/2">
-								<p className="text-base font-medium">{account.income_total.toFixed(2)}</p>
-								<p className="text-base font-medium">{account.expense_total.toFixed(2)}</p>
-								<hr className="my-2 w-24 ml-auto border-gray-300" />
-								<p className="text-base font-medium">${account.remainder.toFixed(2)}</p>
-							</div>
-							</div>
-						))}
-					</div>
+									{/* Numbers on the right */}
+									<div className="text-right w-1/2">
+										<p className="text-base font-medium">{account.income_total.toFixed(2)}</p>
+										<p className="text-base font-medium">{account.expense_total.toFixed(2)}</p>
+										<hr className="my-2 w-24 ml-auto border-gray-300" />
+										<p className="text-base font-medium">${account.remainder.toFixed(2)}</p>
+									</div>
+								</div>
+							))}
+						</div>
 					) : (
-					<div className="bg-white shadow-md rounded-xl p-6 border border-gray-200 flex-1">
-						<h3 className="text-xl font-semibold mb-4">My Accounts</h3>
-						<p className="text-gray-500 text-center">No accounts found.</p>
-					</div>
+						<div className="bg-white shadow-md rounded-xl p-6 border border-gray-200 flex-1">
+							<h3 className="text-xl font-semibold mb-4">My Accounts</h3>
+							<p className="text-gray-500 text-center">No accounts found.</p>
+						</div>
 					)}
 
 					{/* This Month Category Chart */}
 					<div className="bg-white shadow-md rounded-xl p-6 border border-gray-200 flex-1">
-					<h2 className="text-lg font-semibold text-gray-800 mb-4">Spending by Category This Month</h2>
-					<CategoriesAcctsChart transactions={transactions} />
+						<h2 className="text-lg font-semibold text-gray-800 mb-4">Spending by Category This Month</h2>
+						<CategoriesAcctsChart transactions={transactions} />
 					</div>
 					{/* Previous Months Category Chart */}
 					<div className="bg-white shadow-md rounded-xl p-6 border border-gray-200 flex-1">
-					<h2 className="text-lg font-semibold text-gray-800 mb-4">Monthly Spending by Category</h2>
-					<CategoriesMonthsChart transactions={transactions} />
+						<h2 className="text-lg font-semibold text-gray-800 mb-4">Monthly Spending by Category</h2>
+						<CategoriesMonthsChart transactions={transactions} />
 					</div>
-				</div>
-				)}
-
-				<div className="mt-6 flex flex-col lg:flex-row gap-6">
-					{/* Daily expenses per account chart */}
-					<div className="bg-white shadow-md rounded-xl p-6 border border-gray-200 w-full lg:w-2/3 mb-6">
-						<h2 className="text-lg font-semibold text-gray-800 mb-4">Daily Expenses per Account</h2>
-						<ExpPerAcctChart transactions={transactions} />
-					</div>
-
-					{recentTransactions.length > 0 ? (
-					<div className="bg-white shadow-md rounded-xl p-6 border border-gray-200 w-full lg:w-1/3 mb-6">			
-					{/* Display recent transactions */}
-					<h2 className="text-lg font-semibold text-gray-800 mb-4">Recent Transactions</h2>
-					<ul className="space-y-4">
-						{recentTransactions.map((transaction) => (
-							<li key={transaction._id}
-								className="flex justify-between items-start p-2 rounded-md hover:bg-gray-100 transition-transform duration-200 hover:scale-[1.01]">
-								<div>
-									<span className="font-medium text-gray-800">{transaction.name}</span>
-									<div className="text-sm text-gray-600">
-										{transaction.category_id?.name || 'No category'}
-									</div>
-									<div className="text-sm text-gray-500">
-										{transaction.account_id?.name || 'No account'}
-									</div>
-								</div>
-								<div className="text-right">
-									<div className={"text-base font-semibold"}>
-										{transaction.type === 'income' ? '+' : '-'}${transaction.amount.toFixed(2)}
-									</div>
-									<div className="text-xs text-gray-500">
-										{new Date(transaction.date).toLocaleDateString('en-US', {
-											year: 'numeric',
-											month: 'long',
-											day: 'numeric',
-										})}
-									</div>
-								</div>
-							</li>
-						))}
-					</ul>
-				</div>
-			) : (
-				<div className="bg-white shadow-md rounded-xl p-6 border border-gray-200 flex-1 mb-6">
-					<h2 className="text-lg font-semibold text-gray-800 mb-4">Recent Transactions</h2>
-					<p className="text-gray-500 text-center">No recent transactions found.</p>
 				</div>
 			)}
+
+			<div className="mt-6 flex flex-col lg:flex-row gap-6">
+				{/* Daily expenses per account chart */}
+				<div className="bg-white shadow-md rounded-xl p-6 border border-gray-200 w-full lg:w-2/3 mb-6">
+					<h2 className="text-lg font-semibold text-gray-800 mb-4">Daily Expenses per Account</h2>
+					<ExpPerAcctChart transactions={transactions} />
+				</div>
+
+				{recentTransactions.length > 0 ? (
+					<div className="bg-white shadow-md rounded-xl p-6 border border-gray-200 w-full lg:w-1/3 mb-6">
+						{/* Display recent transactions */}
+						<h2 className="text-lg font-semibold text-gray-800 mb-4">Recent Transactions</h2>
+						<ul className="space-y-4">
+							{recentTransactions.map((transaction) => (
+								<li key={transaction._id}
+									className="flex justify-between items-start p-2 rounded-md hover:bg-gray-100 transition-transform duration-200 hover:scale-[1.01]">
+									<div>
+										<span className="font-medium text-gray-800">{transaction.name}</span>
+										<div className="text-sm text-gray-600">
+											{transaction.category_id?.name || 'No category'}
+										</div>
+										<div className="text-sm text-gray-500">
+											{transaction.account_id?.name || 'No account'}
+										</div>
+									</div>
+									<div className="text-right">
+										<div className={"text-base font-semibold"}>
+											{transaction.type === 'income' ? '+' : '-'}${transaction.amount.toFixed(2)}
+										</div>
+										<div className="text-xs text-gray-500">
+											{new Date(transaction.date).toLocaleDateString('en-US', {
+												year: 'numeric',
+												month: 'long',
+												day: 'numeric',
+											})}
+										</div>
+									</div>
+								</li>
+							))}
+						</ul>
+					</div>
+				) : (
+					<div className="bg-white shadow-md rounded-xl p-6 border border-gray-200 flex-1 mb-6">
+						<h2 className="text-lg font-semibold text-gray-800 mb-4">Recent Transactions</h2>
+						<p className="text-gray-500 text-center">No recent transactions found.</p>
+					</div>
+				)}
 			</div>
 
 			<Dashcoins />
